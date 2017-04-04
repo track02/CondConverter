8189a3b9ee15c61bb0b3a2bf2009db3bffb0eb9e
Alex Clarke <a.clarke@runbox.com>
2017-04-04 16:56:34 +0100

    Initial Commit

---
+++ CondConverter/CondConv.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondConverter
+{
+    class CondConv
+    {
+        static void Main(string[] args)
+        {
+
+            string cond_string = "";
+
+
+            //Read in initial string
+            while (true) {
+                string input = Console.ReadLine();
+
+                if (input == "")                
+                    break;               
+                else
+                    cond_string += input;
+                
+           }
+            
+            Console.WriteLine(cond_string);
+
+
+            //Build new string + return
+
+
+
+
+
+
+        }
+    }
+}
